c034137cfeaf6599d1d7088d926c32497533bd97
HWeSek <cichon.adam0205@gmail.com>
2024-03-01 17:45:19 +0100

    w drodze jejjj

--- src/modules/Main.js
+++ src/modules/Main.js
@@ -1,4 +1,5 @@
 import { Scene, AxesHelper, Color, AmbientLight, Raycaster, Vector2 } from 'three';
+import { Tween, Easing } from '@tweenjs/tween.js';
 import Renderer from './renderer';
 import Camera from './camera';
 import { materials } from './Materials';
@@ -44,9 +45,6 @@ const pionki = [
 let pawn_object_list = [];
 let tile_object_list = [];
 
-let playerSide = undefined;
-
-
 const raycaster = new Raycaster();
 const mouseVector = new Vector2()
 let intersects = 0;
@@ -67,16 +65,20 @@ toggleRaycaster(true)
 
 const Game = {
     options: [],
+    selected_pawn: undefined,
+    playerSide: undefined,
+    context: undefined,
+    pawn_direction: undefined,
     setPlayer(side) {
         console.log(side);
         switch (side) {
             case "white":
                 camera.changePosition(-250, 250, 0)
-                playerSide = "white";
+                Game.playerSide = "white";
                 break;
             case "black":
                 camera.changePosition(250, 250, 0)
-                playerSide = "black";
+                Game.playerSide = "black";
                 break;
         }
     },
@@ -113,54 +115,73 @@ const Game = {
         }
     },
     render() {
-        //sconsole.log(playerSide);
-        if (playerSide == "white") {
-            if (intersects.length > 0) {
-                if (intersects[0].object.isPawn && intersects[0].object.color == 1) {
-                    let cords = intersects[0].object.cord;
-                    pawn_object_list.forEach((pawn) => {
-                        pawn.material = pawn.default_material
-                        tile_object_list.forEach((tile) => {
-                            if (tile.color == 0) { tile.material = materials.black_tile }
-                        })
+        switch (Game.playerSide) {
+            case "white":
+                Game.context = [1, 2]
+                Game.pawn_direction = 1
+                break;
+            case "black":
+                Game.context = [2, 1]
+                Game.pawn_direction = -1
+                break;
 
-                    })
-                    intersects[0].object.material = materials.selected_pawn;
-                    let selected_pawn = intersects[0].object
-
+            default:
+                break;
+        }
+        if (intersects.length > 0) {
+            if (intersects[0].object.isPawn && intersects[0].object.color == Game.context[0]) {
+                let cords = intersects[0].object.cord;
+                pawn_object_list.forEach((pawn) => {
+                    pawn.material = pawn.default_material
                     tile_object_list.forEach((tile) => {
+                        if (tile.color == 0) { tile.material = materials.black_tile }
+                    })
 
-                        if ((tile.cord.x == cords.x + 1 && tile.cord.y == cords.y + 1) || (tile.cord.x == cords.x - 1 && tile.cord.y == cords.y + 1)) {
-                            let placement = true;
-                            pawn_object_list.forEach((pawn) => {
-                                if ((tile.cord.x == pawn.cord.x && tile.cord.y == pawn.cord.y)) {
-                                    if (pawn.color == 1) {
-                                        placement = false;
-                                        console.log('nie ma');
-                                    } else if (pawn.color == 2) {
-                                        console.log('tutaj będzie bicie');
-                                    }
+                })
+                intersects[0].object.material = materials.selected_pawn;
+                Game.selected_pawn = intersects[0].object
+
+                tile_object_list.forEach((tile) => {
+
+                    if ((tile.cord.x == cords.x + 1 && tile.cord.y == cords.y + Game.pawn_direction) || (tile.cord.x == cords.x - 1 && tile.cord.y == cords.y + Game.pawn_direction)) {
+                        let placement = true;
+                        pawn_object_list.forEach((pawn) => {
+                            if ((tile.cord.x == pawn.cord.x && tile.cord.y == pawn.cord.y)) {
+                                if (pawn.color == Game.context[0]) {
+                                    placement = false;
+                                    console.log('nie ma');
+                                } else if (pawn.color == Game.context[1]) {
+                                    console.log('tutaj będzie bicie');
                                 }
-                            })
-                            if (placement) {
-                                tile.material = materials.tile_choice;
-                                Game.options.push(tile);
-                                //console.log(Game.options, tile);
                             }
-
+                        })
+                        if (placement) {
+                            tile.material = materials.tile_choice;
+                            Game.options.push(tile);
+                            //console.log(Game.options, tile);
                         }
-                    })
-                    intersects = 0;
-                }
+
+                    }
+                })
+                intersects = 0;
             }
-            if (intersects.length > 0) {
-                if (Game.options.includes(intersects[0].object)) {
-                    console.log('ruch');
-                    Game.options.forEach((tile) => {
-                        tile.material = tile.default_material;
-                    })
-                    Game.options = [];
-                }
+        }
+        if (intersects.length > 0) {
+            if (Game.options.includes(intersects[0].object)) {
+                //console.log('ruch');
+                Game.options.forEach((tile) => {
+                    tile.material = tile.default_material;
+                })
+                let p_x = Game.selected_pawn.cord.x
+                let p_y = Game.selected_pawn.cord.y
+
+                let t_x = intersects[0].object.cord.x
+                let t_y = intersects[0].object.cord.y
+                Game.options = [];
+                //console.log([p_x, p_y], [t_x, t_y]);
+                Game.selected_pawn.material = Game.selected_pawn.default_material
+                Game.selected_pawn.position.set((t_y - 3.5) * 50, 20, (t_x - 3.5) * 50)
+                Game.selected_pawn.cord = { x: parseInt(t_x), y: parseInt(t_y) }
             }
         }
         renderer.render(scene, camera.threeCamera);
